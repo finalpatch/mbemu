6c46b5171c5a6b7bd73e131d9461f6f4a81dcb64
fengli <fengl@fenglis-Mac-Pro.local>
2013-11-12 12:26:57 +1100

    test c++ program with isr installed

--- test.cpp
+++ test.cpp
@@ -1,16 +1,25 @@
 #include <stdio.h>
+#include <stdint.h>
 
 volatile char* io = (char*)0xfffffffc;
-
-const char hello[] = "Hello %s!\n";
+volatile char* intctl = (char*)0xfffffff0;
 
 int main()
 {
-	printf("You name: ");
-	char name[20];
-	scanf("%s", name);
-	printf(hello, name);
+	asm("msrset r5, 0x2");
+	int x;
+	scanf("%d", &x);
+	printf("%d\n", x);
 	return 0;
+}
+
+__attribute__ ((interrupt_handler))
+void isr()
+{
+	*intctl = 0;
+	const static char msg[] = "isr\n";
+	for (const char* p = msg; *p; ++p)
+		*io = *p;
 }
 
 extern "C"
