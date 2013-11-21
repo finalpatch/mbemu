af821a51a2015b85db8fa817a6ea71429bb37c60
fengli <fengl@fenglis-Mac-Pro.local>
2013-11-21 16:41:44 +1100

    exit when program ends (when lcd support is enabled)

--- source/app.d
+++ source/app.d
@@ -1,5 +1,6 @@
 import std.stdio;
 import std.getopt;
+import std.c.stdlib;
 import mbemu.cpu;
 import mbemu.mem;
 import mbemu.elf;
@@ -40,6 +41,7 @@ void main(string[] args)
         while(true)
             handleGdbCommands(cpu);
     }
+    exit(0);
 }
 
 // Local Variables:
