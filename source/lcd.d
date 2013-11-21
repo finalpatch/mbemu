02d79ed5954efdfaa9a14931c26ae040b5866747
fengli <fengl@fenglis-Mac-Pro.local>
2013-11-21 16:20:47 +1100

    use a threaded opengl renderer

--- source/lcd.d
+++ source/lcd.d
@@ -2,24 +2,26 @@ module mbemu.lcd;
 import mbemu.mem;
 import mbemu.fpga;
 import std.bitmanip;
+import core.thread;
+import core.sync.mutex;
+import core.sync.condition;
 
 version(WithLCD)
 {
 	import derelict.sdl2.sdl;
-	static this()
-	{
-		DerelictSDL2.load();
-		SDL_Init(SDL_INIT_VIDEO);
-	}
+	import derelict.opengl3.gl;
 }
 
-class LCD
+class LCD : Thread
 {
 public:
 	this(SDRAM sdram)
 	{
+		super(&run);
 		m_sdram = sdram;
 		init();
+		version(WithLCD)
+			start();
 	}
 	final bool enabled() { return m_enabled; }
 	final void enabled(bool e)
@@ -64,47 +66,73 @@ private:
 	{
 		static immutable width = 320, height = 240;
 		SDL_Window*   win;
-		SDL_Renderer* ren;
-		SDL_Texture*  tex;
+		SDL_GLContext glrc;
+		GLuint        tex;
+		uint[]        buf = new uint[width * height];
+		Condition     cond;
 
 		final void init()
 		{
-			SDL_CreateWindowAndRenderer(width, height, 0, &win, &ren);
-			tex = SDL_CreateTexture(ren, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, width, height);
+			DerelictSDL2.load();
+			DerelictGL.load();
+			SDL_Init(SDL_INIT_VIDEO);
+			win = SDL_CreateWindow("mbemu", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
+			glrc = SDL_GL_CreateContext(win);
+			DerelictGL.reload();
+			SDL_GL_SetSwapInterval(0);
+			glLoadIdentity();
+			glGenTextures(1, &tex);
+			glEnable(GL_TEXTURE_RECTANGLE);
+			glBindTexture(GL_TEXTURE_RECTANGLE, tex);
+			SDL_GL_MakeCurrent(win, null);
+			cond = new Condition(new Mutex());
+		}
+		final void run()
+		{
+			while(true)
+			{
+				synchronized (cond.mutex)
+					cond.wait();
+				update_i();
+			}
 		}
 		final void update()
 		{
+			synchronized (cond.mutex)
+				cond.notify();
+		}
+		final void update_i()
+		{
+			SDL_GL_MakeCurrent(win, glrc);
 			if (m_enabled)
 			{
 				ubyte[] fb =  m_sdram.getBuffer()[m_frameBuffer - m_sdram.base .. m_frameBuffer - m_sdram.base + 256];
 				uint* pixels; int pitch;
-				if (SDL_LockTexture(tex, cast(const(SDL_Rect)*)null, cast(void**)&pixels, &pitch) == 0)
+				for(uint row = 0; row < height; ++row)
 				{
-					pitch /= 4;
-					for(uint row = 0; row < height; ++row)
-					{
-						for(uint col = 0; col < width; ++col)
-						{
-							pixels[col] = lut[fb[row * width + col]];
-						}
-						pixels += pitch;
-					}
-					SDL_UnlockTexture(tex);
+					for(uint col = 0; col < width; ++col)
+						buf[row * width + col] = lut[fb[row * width + col]];
 				}
-				SDL_RenderCopy(ren, tex, null, null);
-				SDL_RenderPresent(ren);
+				glTexImage2D(GL_TEXTURE_RECTANGLE, 0, GL_RGBA, width, height, 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, buf.ptr);
+                glBegin(GL_QUADS);
+                    glTexCoord2i(0, 0);           glVertex2f(-1.0f,  1.0f);
+                    glTexCoord2i(0, height);      glVertex2f(-1.0f, -1.0f);
+                    glTexCoord2i(width, height);  glVertex2f( 1.0f, -1.0f);
+                    glTexCoord2i(width, 0);       glVertex2f( 1.0f,  1.0f);
+				glEnd();
 			}
 			else
 			{
-				SDL_SetRenderDrawColor(ren, 0, 0, 0, 0xff);
-				SDL_RenderFillRect(ren, null);
-				SDL_RenderPresent(ren);
+				glClearColor(0,0,0,1);
+				glClear(GL_COLOR_BUFFER_BIT);
 			}
+			SDL_GL_SwapWindow(win);
 		}
 	}
 	else
 	{
 		final void init() {}
 		final void update() {}
+		final void run() {}
 	}
 }
